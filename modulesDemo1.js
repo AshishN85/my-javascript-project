63d67e1e64a8747f74dd0df505fa524e8cf05d9c
Ashish Naidu <ashish.naidu1985@gmail.com>
2026-02-21 09:05:18 +0530

    Modules Export and Import addition

---
+++ modulesDemo1.js
@@ -0,0 +1,15 @@
+// Extras - #10(1)
+
+// modules in node js
+
+// we can create a module in node js by creating a new file 
+// and exporting the functions or variables that we want to use in other files
+// we can use the require function to import the module in another file
+
+export function add(a, b) {
+    return a + b;
+}
+
+export function subtract(a, b) {
+    return a - b;
+}
--- modulesDemo2.js
+++ modulesDemo2.js
@@ -1,7 +1,12 @@
-// Extras - #10
+// Extras - #10(2)
 
 // modules in node js
 
 // we can create a module in node js by creating a new file 
 // and exporting the functions or variables that we want to use in other files
 // we can use the require function to import the module in another file
+
+import { add, subtract } from './modulesDemo1.js';
+
+console.log(add(5, 3));
+console.log(subtract(5, 3));
